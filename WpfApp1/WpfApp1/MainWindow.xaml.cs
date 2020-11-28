bf913048ededfdde013341cf227f9b8b5729c515
ОБПОУ КГПК <kgpk2@outlook.com>
2020-11-29 01:54:31 +0300

    интерфейс

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Speech.Synthesis;
-using System.Speech.Recognition;
-using System.Speech.AudioFormat;
-using NAudio.Wave;
-using NAudio.FileFormats;
-using NAudio.CoreAudioApi;
-using NAudio;
-using System.Globalization;
+using System.Windows.Threading;
+using WpfApp1.Pages;
+
 
 
 
@@ -31,114 +27,50 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        WaveIn waveIn;
-        
-        WaveFileWriter writer;
-
-        string outputFilename = "имя_файла.wav";
+       
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new Pages.MainPage());
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+
+                MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
             
         }
 
-       
-
-        public bool InvokeRequired { get; private set; }
-
-        [Obsolete]
-        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
         {
-            if (this.InvokeRequired)
+
+        }
+
+        private void MainFrame_ContentRendered_1(object sender, EventArgs e)
+        {
+            var page = (sender as Frame).Content as Page;
+
+            if (page is Pages.MainPage)
             {
-                this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailable), sender, e);
+                BtnBack.Visibility = Visibility.Collapsed;
+
             }
             else
             {
-                //Записываем данные из буфера в файл
-                writer.WriteData(e.Buffer, 0, e.BytesRecorded);
+                BtnBack.Visibility = Visibility.Visible;
+
             }
         }
 
-        private void BeginInvoke(EventHandler<WaveInEventArgs> eventHandler, object sender, WaveInEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
-        }
 
-        //Завершаем запись
-        void StopRecording()
-        {
-            MessageBox.Show("StopRecording");
-            waveIn.StopRecording();
         }
-        //Окончание записи
-        void waveIn_RecordingStopped(object sender, EventArgs e)
-        {
-            if (this.InvokeRequired)
-            {
-                this.BeginInvoke(new EventHandler(waveIn_RecordingStopped), sender, e);
-            }
-            else
-            {
-                waveIn.Dispose();
-                waveIn = null;
-                writer.Close();
-                writer = null;
-            }
-        }
-
-        private void BeginInvoke(EventHandler eventHandler, object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Start_Click(object sender, RoutedEventArgs e)
-        {
-            
-            try
-            {
-                
-                MessageBox.Show("Start Recording");
-                waveIn = new WaveIn();
-                //Дефолтное устройство для записи (если оно имеется)
-                //встроенный микрофон ноутбука имеет номер 0
-                waveIn.DeviceNumber = 0;
-                //Прикрепляем к событию DataAvailable обработчик, возникающий при наличии записываемых данных
-                waveIn.DataAvailable += waveIn_DataAvailable;
-                //Прикрепляем обработчик завершения записи
-                waveIn.RecordingStopped += waveIn_RecordingStopped;
-                //Формат wav-файла - принимает параметры - частоту дискретизации и количество каналов(здесь mono)
-                waveIn.WaveFormat = new WaveFormat(8000, 1);
-                //Инициализируем объект WaveFileWriter
-                writer = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
-                //Начало записи
-                waveIn.StartRecording();
-                
-                
-                
-            }
-            catch (Exception ex)
-            { MessageBox.Show(ex.Message); }
-            
-        }
-
-        private void stop_Click(object sender, RoutedEventArgs e)
-        {
-            if (waveIn != null)
-            {
-                StopRecording();
-            }
-            
-        }
-
-        private void SpeakBtn_Click(object sender, RoutedEventArgs e)
-        {
-            SpeechSynthesizer speak = new SpeechSynthesizer();
-            speak.SelectVoiceByHints(VoiceGender.Male,VoiceAge.Senior);
-            speak.SpeakAsync(SpeakTxt.Text);
-        }
-
-        
     }
     
     
---
+++ WpfApp1/WpfApp1/Pages/Speak.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Speech.AudioFormat;
+using System.Globalization;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Interaction logic for Speak.xaml
+    /// </summary>
+    public partial class Speak : Page
+    {
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer("имя_файла.wav");
+        public Speak()
+        {
+            InitializeComponent();
+
+            
+        }
+        
+        private void SpeakBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SpeechSynthesizer speak = new SpeechSynthesizer();
+            var voiceList = speak.GetInstalledVoices();
+            speak.SelectVoice(voiceList[0].VoiceInfo.Name);
+            speak.Rate = 0;
+            speak.SpeakAsync(SpeakTxt.Text);
+        }
+
+        private void RecordBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SpeechRecognitionEngine _rec = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+            _rec.LoadGrammar(new DictationGrammar());
+            _rec.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+            _rec.SetInputToDefaultAudioDevice();
+            _rec.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            SpeakTxt.Text = e.Result.Text;
+        }
+    }
+}
